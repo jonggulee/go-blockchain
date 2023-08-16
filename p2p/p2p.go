ff958a692d3ccb3bdfe7b68340e3b07cff013776
Jonggu Lee <jg.jake.lee@gmail.com>
2023-08-16 20:57:23 +0900

    feat: Add chat connections

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -1,7 +1,6 @@
 package p2p
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/websocket"
@@ -10,15 +9,25 @@ import (
 
 var upgrader = websocket.Upgrader{}
 
+var conns []*websocket.Conn
+
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 
 	conn, err := upgrader.Upgrade(rw, r, nil)
+	conns = append(conns, conn)
 	utils.HandleErr(err)
 
 	for {
 		_, p, err := conn.ReadMessage()
-		utils.HandleErr(err)
-		fmt.Printf("%s\n\n", p)
+		if err != nil {
+			break
+		}
+
+		for _, aConn := range conns {
+			if aConn != conn {
+				utils.HandleErr(aConn.WriteMessage(websocket.TextMessage, p))
+			}
+		}
 	}
 }
