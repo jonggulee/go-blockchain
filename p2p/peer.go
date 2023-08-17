ecdfc4d07cb1eb8a19e6d84f2c6cab8116d37081
Jonggu Lee <jg.jake.lee@gmail.com>
2023-08-17 16:35:53 +0900

    feat: Add channel

--- p2p/peer.go
+++ p2p/peer.go
@@ -9,14 +9,35 @@ import (
 var Peers map[string]*peer = make(map[string]*peer)
 
 type peer struct {
-	conn *websocket.Conn
+	conn  *websocket.Conn
+	inbox chan []byte
+}
+
+func (p *peer) read() {
+	for {
+		_, m, err := p.conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		fmt.Printf("%s\n", m)
+	}
+}
+
+func (p *peer) write() {
+	for {
+		m := <-p.inbox
+		p.conn.WriteMessage(websocket.TextMessage, m)
+	}
 }
 
 func initPeer(conn *websocket.Conn, address, port string) *peer {
 	p := &peer{
 		conn,
+		make(chan []byte),
 	}
 	key := fmt.Sprintf("%s:%s", address, port)
+	go p.read()
+	go p.write()
 	Peers[key] = p
 	return p
 }
