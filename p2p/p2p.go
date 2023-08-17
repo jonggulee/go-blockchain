ab7aab6b73d4d2aece731a8b3ec99788efa22cc9
Jonggu Lee <jg.jake.lee@gmail.com>
2023-08-17 10:17:23 +0900

    feat: Add initPeer

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -1,6 +1,7 @@
 package p2p
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/websocket"
@@ -9,25 +10,17 @@ import (
 
 var upgrader = websocket.Upgrader{}
 
-var conns []*websocket.Conn
-
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
-	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-
-	conn, err := upgrader.Upgrade(rw, r, nil)
-	conns = append(conns, conn)
-	utils.HandleErr(err)
-
-	for {
-		_, p, err := conn.ReadMessage()
-		if err != nil {
-			break
-		}
-
-		for _, aConn := range conns {
-			if aConn != conn {
-				utils.HandleErr(aConn.WriteMessage(websocket.TextMessage, p))
-			}
-		}
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
 	}
+	conn, err := upgrader.Upgrade(rw, r, nil)
+	utils.HandleErr(err)
+	initPeer(conn, "xx", "xx")
+}
+
+func AddPeer(address, port string) {
+	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws", address, port), nil)
+	utils.HandleErr(err)
+	initPeer(conn, address, port)
 }
