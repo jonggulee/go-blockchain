b9bda6233d2e685c1ad3ade7178996a94ef6bf75
Jonggu Lee <jg.jake.lee@gmail.com>
2023-09-01 16:06:04 +0900

    feat: Add Test difficutly

--- blockchain/chain_test.go
+++ blockchain/chain_test.go
@@ -1,6 +1,7 @@
 package blockchain
 
 import (
+	"reflect"
 	"sync"
 	"testing"
 
@@ -53,4 +54,101 @@ func TestBlockchain(t *testing.T) {
 			t.Errorf("Blockchain() should restore a blockchain with a height of %d, got %d", 2, bc.Height)
 		}
 	})
+}
+
+func TestBlocks(t *testing.T) {
+	fakeBlocks := 0
+	dbStorage = fakeDB{
+		fakeFindBlock: func() []byte {
+			var b *Block
+			if fakeBlocks == 0 {
+				b = &Block{
+					Height:   2,
+					PrevHash: "x",
+				}
+			}
+			if fakeBlocks == 1 {
+				b = &Block{
+					Height: 1,
+				}
+			}
+			fakeBlocks++
+			return utils.ToBytes(b)
+		},
+	}
+	bc := &blockchain{}
+	blocks := Blocks(bc)
+	if reflect.TypeOf(blocks) != reflect.TypeOf([]*Block{}) {
+		t.Error("Blocks() should return a slice of blocks")
+	}
+}
+
+func TestFindTx(t *testing.T) {
+	t.Run("Tx found", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeFindBlock: func() []byte {
+				b := &Block{
+					Height:       2,
+					Transactions: []*Tx{},
+				}
+				return utils.ToBytes(b)
+			},
+		}
+		tx := FindTx(&blockchain{NewestHash: "x"}, "test")
+		if tx != nil {
+			t.Error("Tx should be not found.")
+		}
+	})
+	t.Run("Tx should be found", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeFindBlock: func() []byte {
+				b := &Block{
+					Height: 2,
+					Transactions: []*Tx{
+						{ID: "test"},
+					},
+				}
+				return utils.ToBytes(b)
+			},
+		}
+		tx := FindTx(&blockchain{NewestHash: "x"}, "test")
+		if tx == nil {
+			t.Error("Tx should be found.")
+		}
+	})
+}
+
+func TestGetDifficulty(t *testing.T) {
+	blocks := []*Block{
+		{PrevHash: "x"},
+		{PrevHash: "x"},
+		{PrevHash: "x"},
+		{PrevHash: "x"},
+		{PrevHash: ""},
+	}
+	fakeBlock := 0
+	dbStorage = fakeDB{
+		fakeFindBlock: func() []byte {
+			defer func() {
+				fakeBlock++
+			}()
+			return utils.ToBytes(blocks[fakeBlock])
+		},
+	}
+	type test struct {
+		height int
+		want   int
+	}
+	tests := []test{
+		{height: 0, want: defaultDifficulty},
+		{height: 2, want: defaultDifficulty},
+		{height: 5, want: 3},
+	}
+	for _, tc := range tests {
+		bc := &blockchain{Height: tc.height, CurrentDifficulty: defaultDifficulty}
+		got := getDifficulty(bc)
+		if got != tc.want {
+			t.Errorf("getDifficulty() should return %d, got %d", tc.want, got)
+		}
+	}
 }
